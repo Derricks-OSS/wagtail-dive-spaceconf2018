67f95d9ef6830445c00a319f2db5a0bf240efd90
coderrick <derrick.duncan@temple.edu>
2018-06-15 15:00:05 +0000

    Added content panel

--- workshop/home/models.py
+++ workshop/home/models.py
@@ -8,3 +8,7 @@ from wagtail.core.models import Page
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+    
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
