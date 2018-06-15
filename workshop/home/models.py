6fc30d7d909e4641ea10de0239d45cc44da75177
coderrick <derrick.duncan@temple.edu>
2018-06-15 14:23:01 +0000

    intial commit

---
+++ workshop/home/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+from wagtail.core.models import Page
+
+
+class HomePage(Page):
+    body = RichTextField(blank=True)
